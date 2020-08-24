61d73e79f4ee8c70027bb86f849284ad29ac049f
victorruand <victorruan135@gmail.com>
2020-08-24 09:57:55 -0300

    Fiz alterações no projeto em geral, direcionando-o para um transcritor de voz-texto.

--- ouvir1.py
+++ ouvir1.py
@@ -1,15 +1,28 @@
 import speech_recognition as sr
 
+# parte de ouvir e reconhecer
 def ouvir_microfone():
-    microfone = sr.Recognizer()
-    with sr.Microphone() as source:
-        microfone.adjust_for_ambient_noise(source)
-        print("Diga alguma coisa: ")
-        audio = microfone.listen(source)
-    try:
-        frase = microfone.recognize_google(audio, key="AIzaSyDRdSN1VaRW27HxA68rZW5FesS2qoPD8", language='pt-BR', show_all=True)
-        print("Você disse: " + frase)
-    except sr.UnKownValueError:
-        print("Não entendi")
-        return frase
+    on = True
+    while on:
+        microfone = sr.Recognizer()
+        with sr.Microphone() as source:
+            #ajuste para ruidos no ambiente
+            microfone.adjust_for_ambient_noise(source)
+            print('"Terminar aula" para SAIR')
+            audio = microfone.listen(source)
+            try:
+                frase = microfone.recognize_google(audio, language="pt-BR")
+                print("Foi dito:    " + frase)
+                if frase == 'terminar aula':
+                    break
+            except sr.UnknownValueError:
+                ouvir_microfone()
+            #return frase
+    else:
+        ouvir_microfone()
+
+
+
+# inicialização da função:
 ouvir_microfone()
+
